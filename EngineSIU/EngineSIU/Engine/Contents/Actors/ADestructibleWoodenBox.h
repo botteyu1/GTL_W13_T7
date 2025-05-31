0bcabd0994e692a7d0ad55e0a227932251945623
YuJunSang <botteyu1@gmail.com>
2025-05-31 19:45:09 +0900

    feat: add AADestructibleWoodenBox

---
+++ EngineSIU/EngineSIU/Engine/Contents/Actors/ADestructibleWoodenBox.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "GameFramework/Actor.h"
+
+
+class AADestructibleWoodenBox : public AActor
+{
+    DECLARE_CLASS(AADestructibleWoodenBox, AActor)
+
+public:
+    AADestructibleWoodenBox() = default;
+    virtual ~AADestructibleWoodenBox() override = default;
+    
+};
